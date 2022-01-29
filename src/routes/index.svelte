b604a838797859be2c8488b390aed6a4752f6c4b
Chathura Harshanga <38973288+chathuraHarshanga@users.noreply.github.com>
2022-01-29 16:51:45 +0530

    display movies

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,22 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script context="module">
+	export async function load({ fetch }) {
+		const res = await fetch(
+			`https://api.themoviedb.org/3/movie/popular?api_key=8c5b5e30e504139fd6699ba73c13557e&language=en-US&page=1`
+		);
+		const data = await res.json();
+		if (res.ok) {
+			return {
+				props: { popular: data.results }
+			};
+		}
+	}
+</script>
+
+<script>
+	import PopularMovies from '../components/PopularMovies.svelte';
+	export let popular;
+</script>
+
+<section>
+	<PopularMovies {popular} />
+</section>
